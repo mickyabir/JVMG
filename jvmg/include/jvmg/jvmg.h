69e7cb046f458451c0f5a36b9b3c004f620c2497
Micky Abir <micky.abir@gmail.com>
2024-01-31 01:16:54 -0800

    adds jvmg.cpp and jvmg.h (easier for CMake and provides core access, TODO)

---
+++ jvmg/include/jvmg/jvmg.h
@@ -0,0 +1,10 @@
+//
+// Created by Micky on 1/31/2024.
+//
+
+#ifndef JVMG_JVMG_H
+#define JVMG_JVMG_H
+
+#define JVMG_VERSION "0.1.0"
+
+#endif //JVMG_JVMG_H
---
+++ jvmg/src/jvmg.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Micky on 1/31/2024.
+//
+
+#include <string>
+#include "jvmg/jvmg.h"
+
+std::string getJVMGVersion() { return JVMG_VERSION; }
